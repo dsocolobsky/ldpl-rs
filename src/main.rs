1ac7ea65fbbfcea67cb19b7317be049916d99d74
Dylan Socolobsky <dsocolobsky@gmail.com>
2024-07-18 17:05:31 -0300

    Refactor into struct

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-use std::collections::HashMap;
-use pest::Parser;
+use pest::iterators::Pair;
 use pest_derive::Parser;
+use std::collections::HashMap;
 use std::env;
 use std::fs;
-use pest::iterators::Pair;
+use pest::Parser;
 
 #[derive(Parser)]
 #[grammar = "ldpl.pest"]
@@ -21,129 +21,130 @@ enum VariableValue {
     Text(String),
 }
 
-fn handle_display(display_pair: pest::iterators::Pair<Rule>, variable_values: &HashMap<&str, VariableValue>) {
-    for display_pair in display_pair.into_inner() {
-        match display_pair.as_rule() {
-            Rule::string => {
-                print!("{}", display_pair.as_str());
-            }
-            Rule::identifier => {
-                let variable_name = display_pair.as_str();
-                if variable_name == "clrf" {
-                    print!("\n");
-                    continue;
-                }
-                let variable_value = variable_values.get(variable_name).
-                    expect(&format!("variable {} not found", variable_name));
-                match variable_value {
-                    VariableValue::Number(value) => {
-                        print!("{}", value);
-                    }
-                    VariableValue::Text(value) => {
-                        print!("{}", value);
-                    }
-                }
-            }
-            _ => panic!("Unknown display type"),
-        }
-    }
+struct Interpreter<'a> {
+    variable_values: HashMap<&'a str, VariableValue>,
+    variable_types: HashMap<&'a str, VariableType>,
 }
 
-fn handle_comparison(comparison: pest::iterators::Pair<Rule>, variable_values: &HashMap<&str, VariableValue>) -> bool {
-    let mut inner_rules = comparison.into_inner();
-    let left = inner_rules.next().unwrap();
-    let op = inner_rules.next().unwrap();
-    let right = inner_rules.next().unwrap();
+impl<'a> Interpreter<'a> {
+    fn new() -> Interpreter<'a> {
+        Interpreter {
+            variable_values: HashMap::new(),
+            variable_types: HashMap::new(),
+        }
+    }
 
-    let left_val = handle_value(variable_values, left);
-    let right_val = handle_value(variable_values, right);
+    fn handle_value(&self, pair: Pair<Rule>) -> VariableValue {
+        match pair.as_rule() {
+            Rule::number => VariableValue::Number(pair.as_str().parse().unwrap()),
+            Rule::string => VariableValue::Text(pair.as_str().to_string()),
+            Rule::identifier => {
+                let variable_name = pair.as_str();
+                let variable_value = self.variable_values
+                    .get(variable_name)
+                    .expect(&format!("variable {} not found", variable_name));
+                variable_value.clone()
+            }
+            _ => panic!("Unknown variable type"),
+        }
+    }
 
-    match op.as_str() {
-        "is equal to" => {
-            match (left_val, right_val) {
+    fn handle_comparison(
+        &self,
+        comparison: Pair<Rule>
+    ) -> bool {
+        let mut inner_rules = comparison.into_inner();
+        let left = inner_rules.next().unwrap();
+        let op = inner_rules.next().unwrap();
+        let right = inner_rules.next().unwrap();
+
+        let left_val = self.handle_value(left);
+        let right_val = self.handle_value(right);
+
+        match op.as_str() {
+            "is equal to" => match (left_val, right_val) {
                 (VariableValue::Number(left), VariableValue::Number(right)) => left == right,
                 _ => panic!("Cannot compare different types"),
-            }
-        },
-        "is not equal to" => {
-            match (left_val, right_val) {
+            },
+            "is not equal to" => match (left_val, right_val) {
                 (VariableValue::Number(left), VariableValue::Number(right)) => left != right,
                 _ => panic!("Cannot compare different types"),
-            }
-        },
-        "is greater than" => {
-            match (left_val, right_val) {
+            },
+            "is greater than" => match (left_val, right_val) {
                 (VariableValue::Number(left), VariableValue::Number(right)) => left > right,
                 _ => panic!("Cannot compare different types"),
-            }
-        },
-        "is less than" => {
-            match (left_val, right_val) {
+            },
+            "is less than" => match (left_val, right_val) {
                 (VariableValue::Number(left), VariableValue::Number(right)) => left < right,
                 _ => panic!("Cannot compare different types"),
-            }
-        },
-        "is greater than or equal to" => {
-            match (left_val, right_val) {
+            },
+            "is greater than or equal to" => match (left_val, right_val) {
                 (VariableValue::Number(left), VariableValue::Number(right)) => left >= right,
                 _ => panic!("Cannot compare different types"),
-            }
-        },
-        "is less than or equal to" => {
-            match (left_val, right_val) {
+            },
+            "is less than or equal to" => match (left_val, right_val) {
                 (VariableValue::Number(left), VariableValue::Number(right)) => left <= right,
                 _ => panic!("Cannot compare different types"),
-            }
-        },
-        _ => panic!("Unhandled comparison for now"),
-    }
-}
-
-fn handle_value(variable_values: &HashMap<&str, VariableValue>, pair: Pair<Rule>) -> VariableValue {
-    match pair.as_rule() {
-        Rule::number => VariableValue::Number(pair.as_str().parse().unwrap()),
-        Rule::string => VariableValue::Text(pair.as_str().to_string()),
-        Rule::identifier => {
-            let variable_name = pair.as_str();
-            let variable_value = variable_values.get(variable_name).
-                expect(&format!("variable {} not found", variable_name));
-            variable_value.clone()
-        }
-        _ => panic!("Unknown variable type"),
-    }
-}
-
-fn handle_if_guard(if_guard: pest::iterators::Pair<Rule>, variable_values: &HashMap<&str, VariableValue>) -> bool {
-    let mut inner_rules = if_guard.into_inner();
-    let comparison_rules = inner_rules.next().unwrap();
-    handle_comparison(comparison_rules, variable_values)
-}
-
-fn main() {
-    // map from variable name to type
-    let mut variable_types: HashMap<&str, VariableType> = HashMap::new();
-    let mut variable_values: HashMap<&str, VariableValue> = HashMap::new();
-
-    let path = env::current_dir().unwrap();
-    println!("The current directory is {}", path.display());
-    let unparsed_file =
-        fs::read_to_string("test.ldpl").expect("Should have been able to read the file");
-    let expressions = LDPLParser::parse(Rule::file, &unparsed_file)
-        .expect("unsuccessful parse");
-    for expression in expressions {
-        match expression.as_rule() {
-            Rule::display => {
-                handle_display(expression, &variable_values);
             },
+            _ => panic!("Unhandled comparison for now"),
+        }
+    }
+
+    fn handle_if_guard(&mut self, if_guard: Pair<Rule>) -> bool {
+        let mut inner_rules = if_guard.into_inner();
+        let comparison_rules = inner_rules.next().unwrap();
+        self.handle_comparison(comparison_rules)
+    }
+
+    fn handle_display(&self, display_pair: Pair<Rule>) {
+        for display_pair in display_pair.into_inner() {
+            match display_pair.as_rule() {
+                Rule::string => {
+                    print!("{}", display_pair.as_str());
+                }
+                Rule::identifier => {
+                    let variable_name = display_pair.as_str();
+                    if variable_name == "clrf" {
+                        print!("\n");
+                        continue;
+                    }
+                    let variable_value = self.variable_values
+                        .get(variable_name)
+                        .expect(&format!("variable {} not found", variable_name));
+                    match variable_value {
+                        VariableValue::Number(value) => {
+                            print!("{}", value);
+                        }
+                        VariableValue::Text(value) => {
+                            print!("{}", value);
+                        }
+                    }
+                }
+                _ => panic!("Unknown display type"),
+            }
+        }
+    }
+
+    fn handle_pair(
+        &mut self,
+        pair: Pair<'a, Rule>
+    ) {
+        match pair.as_rule() {
+            Rule::display => {
+                self.handle_display(pair);
+            }
             Rule::if_statement => {
-                let mut inner_rules = expression.into_inner();
+                let mut inner_rules = pair.into_inner();
                 let if_guard = inner_rules.next().unwrap();
+                let guard_result = self.handle_if_guard(if_guard);
+                if !guard_result {
+                    return;
+                }
                 let if_block = inner_rules.next().unwrap();
-                let guard_result = handle_if_guard(if_guard, &variable_values);
-                println!("{:?}", guard_result);
+                self.handle_pair(if_block);
             }
             Rule::store => {
-                let mut inner_rules = expression.into_inner();
+                let mut inner_rules = pair.into_inner().clone();
                 let variable_value = inner_rules.next().unwrap();
                 let variable_value = match variable_value.as_rule() {
                     Rule::number => VariableValue::Number(variable_value.as_str().parse().unwrap()),
@@ -151,10 +152,10 @@ fn main() {
                     _ => panic!("Unknown variable type"),
                 };
                 let variable_name = inner_rules.next().unwrap().as_str();
-                variable_values.insert(variable_name, variable_value);
+                self.variable_values.insert(variable_name, variable_value);
             }
             Rule::variable_declaration => {
-                let mut inner_rules = expression.into_inner();
+                let mut inner_rules = pair.into_inner().clone();
                 let variable_name = inner_rules.next().unwrap().as_str();
                 let variable_type = inner_rules.next().unwrap().as_str();
                 let variable_type = match variable_type {
@@ -162,13 +163,23 @@ fn main() {
                     "text" => VariableType::Text,
                     _ => panic!("Unknown variable type"),
                 };
-                variable_types.insert(variable_name, variable_type);
-            },
-            Rule::EOI => {
+                self.variable_types.insert(variable_name, variable_type);
             }
-            _ => panic!(
-                "Unknown expression type: {:?}", expression.as_rule(),
-            ),
+            Rule::EOI => {}
+            _ => panic!("Unknown expression type: {:?}", pair.as_rule(),),
         }
+    }
+}
+
+fn main() {
+    let mut interpreter = Interpreter::new();
+
+    let path = env::current_dir().unwrap();
+    println!("The current directory is {}", path.display());
+    let unparsed_file =
+        fs::read_to_string("test.ldpl").expect("Should have been able to read the file");
+    let expressions = LDPLParser::parse(Rule::file, &unparsed_file).expect("unsuccessful parse");
+    for exp in expressions {
+        interpreter.handle_pair(exp);
     }
 }
